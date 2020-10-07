4f18c8a24ad12941e54c9b7bbf0d1ffd8cd78c18
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-10-07 17:40:24 -0500

    javascript bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,26 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const webpack = require("webpack");
 const path = require('path');
 
 
 module.exports = {
-    entry: './assets/js/script.js', output: {
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/events.js",
+        tickets: "./assets/js/tickets.js"
+    },
+    output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
-    }, plugins: [
+        filename: '[name].bundle.js'
+    },
+    plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static",// the report outputs to an HTML file in the dist folder
+        })
     ], mode: 'development'
 };
